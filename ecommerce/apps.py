6400959606283c106558292255da00669aa5089a
JBeggs <django.python.pro@gmail.com>
2025-12-06 14:37:36 +0200

    adding riverside herald user to start getting content

--- ecommerce/apps.py
+++ ecommerce/apps.py
@@ -5,4 +5,11 @@ class EcommerceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ecommerce'
     verbose_name = 'E-commerce Multi-Tenant'
+    
+    def ready(self):
+        """Import admin when app is ready."""
+        try:
+            import ecommerce.admin  # NOQA
+        except ImportError:
+            pass
 
--- fcm/apps.py
+++ fcm/apps.py
@@ -5,4 +5,11 @@ class FcmConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fcm'
     verbose_name = 'Firebase Cloud Messaging'
+    
+    def ready(self):
+        """Import admin when app is ready."""
+        try:
+            import fcm.admin  # NOQA
+        except ImportError:
+            pass
 

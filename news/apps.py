f09cc505f84f293a20084bca5ab4ad4e81721b69
JBeggs <django.python.pro@gmail.com>
2025-12-06 14:18:39 +0200

    adding riverside herald user to start getting content

--- news/apps.py
+++ news/apps.py
@@ -5,4 +5,11 @@ class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news'
     verbose_name = 'News Platform'
+    
+    def ready(self):
+        """Import admin when app is ready."""
+        try:
+            import news.admin  # NOQA
+        except ImportError:
+            pass
 
